dcaa000be289e76ee73f9ad5887f5a1803a6f6dc
Mrini <myanh@moneylover.me>
2017-12-20 17:14:57 +0700

    foo colorrr

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ exports.anotherMsg = function(msg, color, repeatTimes) {
 }
 
 exports.welcomeMsg = function() {
-console.log("ふぁんど".green);
+console.log("ふぁんど".cyan);
 }
 
 exports.welcomeMsg = function(color) {
